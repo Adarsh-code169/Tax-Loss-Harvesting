76ac4afaa40081ea11bdd9ff7d470e1825a7f09f
Adarsh-code169 <adarshshukla@example.com>
2026-04-20 13:00:00 +0530

    feat: setup react entry point

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { HarvestingProvider } from './context/HarvestingContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HarvestingProvider>
+      <App />
+    </HarvestingProvider>
+  </React.StrictMode>,
+)
